14553dc87791dfa835c44edd0118329d8773d385
An Unconditional Lover of Wisdom <140563635+bigbadcyborg@users.noreply.github.com>
2024-11-28 12:14:35 -0600

    Add files via upload

---
+++ network.bat
@@ -0,0 +1,32 @@
+ @echo off
+ mode 38,20
+ title Network Stats
+ echo Loading Network Data...
+ :loop
+ for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "SSID" ^| findstr /v "BSSID"') do set ssid=%%a
+ for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "Description"') do set adapter=%%a
+ for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "State"') do set state=%%a
+ for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "Signal"') do set signal=%%a
+ ping -n 3 8.8.8.8>%temp%\ping.txt
+ for /f "tokens=4 delims==" %%a in ('type %temp%\ping.txt ^| find "Average"') do set ping=%%a
+ for /f "tokens=10 delims= " %%a in ('type %temp%\ping.txt ^| find "Lost"') do set ploss=%%a
+ for /f "tokens=2 delims= " %%a in ('netstat -e ^| find "Bytes"') do set rbytes=%%a
+ for /f "tokens=3 delims= " %%a in ('netstat -e ^| find "Bytes"') do set sbytes=%%a
+
+
+
+ cls
+ echo  Network:
+ echo  --------
+ echo  SSID:%ssid%
+ echo  NIC:%adapter%
+ echo  State:%state%
+ echo  Signal:%signal%
+ echo.
+ echo  Speed:
+ echo  --------
+ echo %ping%
+ echo  Packet Loss: %ploss% Bytes
+ echo  Recieved: %rbytes% Bytes
+ echo  Sent: %sbytes% Bytes
+ goto loop
